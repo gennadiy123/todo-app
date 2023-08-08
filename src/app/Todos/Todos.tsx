291166a4d1ad6826061f42d2abf4e05ecd85d29a
gennadiy123 <shelukhin_g@ukr.net>
2023-08-08 12:49:44 +0300

    implemented add and delete tasks

--- src/app/Todos/Todos.tsx
+++ src/app/Todos/Todos.tsx
@@ -1,16 +1,21 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { nanoid } from "nanoid";
-
-let tasks = ["Repair a car", "Find a job"];
 
 export const Todos = () => {
   const [task, setTask] = useState<{ name: string; id: string }[]>([]);
 
+  let storageTasks: string | null = null;
+
   useEffect(() => {
-    localStorage.getItem("todos");
-  }, []);
+    storageTasks = localStorage.getItem("todos");
+    storageTasks && setTask(JSON.parse(storageTasks));
+  }, [])
+
+  useEffect(() => {
+     storageTasks && setTask(JSON.parse(storageTasks));
+  }, [storageTasks]);
 
   const onSubmit: React.FormEventHandler<HTMLFormElement> = (
     e: React.FormEvent<HTMLFormElement>
@@ -18,24 +23,41 @@ export const Todos = () => {
     e.preventDefault();
     const formElement = e.currentTarget as HTMLFormElement;
     const inputElement = formElement[0] as HTMLInputElement;
+    const newTask = { name: inputElement.value, id: nanoid() };
+
     if (task.some((el) => el.name === inputElement.value)) {
       alert("This task is already exists!");
     } else {
-      setTask([...task, { name: inputElement.value, id: nanoid() }]);
+      setTask((task) => [...task, newTask]);
+      localStorage.setItem("todos", JSON.stringify([...task, newTask]));
     }
 
     formElement.reset();
   };
 
+  const onRemoveTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const {name} = e.target as HTMLButtonElement;
+
+    setTask((task) => task.filter((el) => el.id !== name));
+    storageTasks = JSON.stringify(task.filter((el) => el.id !== name));
+    localStorage.setItem("todos", storageTasks);
+  };
+
   return (
     <>
+      <h2>All Tasks</h2>
       <form onSubmit={onSubmit}>
         <input placeholder="Input your task" />
         <button type="submit">Add task</button>
       </form>
       <ol>
-        {task.map((el) => (
-          <li key={el.id}>{el.name}</li>
+        {task.map(({ id, name }) => (
+          <div key={id}>
+            <li>{name}</li>
+            <button name={id} onClick={onRemoveTask}>
+              Remove task
+            </button>
+          </div>
         ))}
       </ol>
     </>
